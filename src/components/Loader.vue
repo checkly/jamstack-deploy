00ecc555c1219398486c96eee7782a86984ea32b
Ignacio Anaya <nacho@checklyhq.com>
2021-04-02 12:22:50 -0300

    chore: remove sub text from loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -3,9 +3,6 @@
     class="flex flex-col mt-16 justify-center items-center text-xl font-semibold text-black"
   >
     <h1 class="text-black font-semibold text-2xl">Loading {{ text }}...</h1>
-    <span class="text-gray text-base">
-      Getting checks from Checkly Public API
-    </span>
     <img
       class="mt-5"
       src="/images/raccoon-loading.gif"
