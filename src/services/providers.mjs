ba72e69fbe920bcb2acb8abb9206e0cc95e63407
Ignacio Anaya <nacho@checklyhq.com>
2022-06-20 01:01:55 -0300

    feat: add new gitlab provider

---
+++ src/services/providers.mjs
@@ -0,0 +1,135 @@
+export const provdersList = {
+  amplify: {
+    link: "https://aws.amazon.com/amplify",
+    displayName: "AWS Amplify",
+    checkId: "92cc33ad-246e-4c56-bcb3-6722e3fb2107",
+    logo: {
+      ext: "png",
+      style: "width: 220px;"
+    }
+  },
+  aws: {
+    link: "https://aws.amazon.com",
+    checkUrl: "https://s3.jamstackdeploy.com",
+    displayName: "AWS S3 + CloudFront",
+    checkId: "553c30f5-51ab-4cb4-98c4-44c9704331ed",
+    logo: {
+      ext: "png",
+      style: "width: 80px;"
+    }
+  },
+  azure: {
+    link: "https://azure.microsoft.com",
+    displayName: "Microsoft Azure",
+    checkId: "66f1dc8d-9717-4567-adf0-8c414298ce6e",
+    logo: {
+      ext: "png",
+      style: "width: 160px;"
+    }
+  },
+  bip: {
+    link: "https://bip.sh",
+    checkUrl: "https://jamstackdeploy.bip.sh",
+    displayName: "BIP",
+    checkId: "bc8e096a-5c29-4f00-8224-033f4c5de5b7",
+    logo: {
+      ext: "png",
+      style: "width: 80px;"
+    }
+  },
+  cloudflare: {
+    link: "https://pages.cloudflare.com",
+    displayName: "Cloudflare",
+    checkId: "73783e95-f625-42d2-ba1f-945c8301c199",
+    logo: {
+      ext: "svg",
+      style: "width: 150px;"
+    }
+  },
+  do: {
+    link: "https://digitalocean.com",
+    displayName: "Digital Ocean",
+    checkId: "54175d6d-650d-49f9-833f-941eedecc10a",
+    logo: {
+      ext: "svg",
+      style: "width: 220px;"
+    }
+  },
+
+  firebase: {
+    link: "https://firebase.google.com",
+    displayName: "Firebase",
+    checkId: "fce0a5f4-e6e1-4f8c-9348-d0da625a5bee",
+    logo: {
+      ext: "png",
+      style: "width: 160px; height: 45px"
+    }
+  },
+
+  github: {
+    link: "https://pages.github.com",
+    displayName: "GitHub Pages",
+    checkId: "41b06d68-1943-4abd-8ba8-79bb9c322c5e",
+    logo: {
+      ext: "png",
+      style: "width: 140px; height: 57px"
+    }
+  },
+
+  gitlab: {
+    link: "https://gitlab.com",
+    displayName: "Gitlab",
+    checkId: "0d0a59e5-f54f-4ff9-811f-1a07c91f2df2",
+    logo: {
+      ext: "png",
+      style: "width: 180px;"
+    }
+  },
+  hostman: {
+    link: "https://hostman.com",
+    checkUrl: "https://jamstack-deploy-demo-app.hostman.site",
+    displayName: "Hostman",
+    checkId: "13c33562-815c-49c4-8eeb-5589a7ed7f12",
+    logo: {
+      ext: "svg",
+      style: "width: 180px;"
+    }
+  },
+  netlify: {
+    link: "https://netlify.com",
+    displayName: "Netlify",
+    checkId: "85195bd0-b2ee-4a24-abed-4337dc841004",
+    logo: {
+      ext: "png",
+      style: "width: 140px; height: 38px"
+    }
+  },
+  render: {
+    link: "https://render.com",
+    displayName: "Render",
+    checkId: "e1cac774-7253-4178-b2d0-d15b264a4c09",
+    logo: {
+      ext: "svg",
+      style: "width: 160px; height: 34px"
+    }
+  },
+  surge: {
+    link: "https://surge.sh",
+    checkUrl: "http://surge.jamstackdeploy.com",
+    displayName: "Surge",
+    checkId: "79c8c050-5769-4ab0-a109-c367d82c26cf",
+    logo: {
+      ext: "svg",
+      style: "width: 64px; height: 64px"
+    }
+  },
+  vercel: {
+    link: "https://vercel.com",
+    displayName: "Vercel",
+    checkId: "351ce608-6753-4734-94ae-1d723c940326",
+    logo: {
+      ext: "svg",
+      style: "width: 131px; height: 30px"
+    }
+  }
+};
